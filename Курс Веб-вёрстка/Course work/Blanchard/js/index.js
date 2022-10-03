397089e010d97c551dd9a3942955be1bedc61d26
Yulia Vasileva <Alleks_V@mail.ru>
2022-10-03 13:34:23 +0300

    Pre-final.1 version

--- Курс Веб-вёрстка/Course work/Blanchard/js/index.js
+++ Курс Веб-вёрстка/Course work/Blanchard/js/index.js
@@ -1,6 +1,79 @@
 window.addEventListener('DOMContentLoaded', function () {
 
-  //Selector
+  // //Header Dropdown Scroll-------------------------
+
+  // Array.prototype.forEach.call(
+  //   document.querySelectorAll('.data-simplebar'),
+  //   el => new SimpleBar()
+  // );
+
+  //Header Dropdown-----------------------------------
+
+  let headerLink = document.querySelectorAll('.header_bottom_list_item_link');
+  // let headerDropdown = document.querySelectorAll('.header_bottom_list_item_dropdown');
+
+  headerLink.forEach(function(element){
+    element.addEventListener('click', function(e){
+      const path = e.currentTarget.dataset.path;
+
+      headerLink.forEach(function(link){
+        link.classList.remove('header_bottom_list_item_link--active');
+      });
+      e.currentTarget.classList.add('header_bottom_list_item_link--active');
+
+      // headerDropdown.forEach(function(element){
+      //   element.classList.remove('header_bottom_list_item_dropdown--active');
+      // });
+      document.querySelector(`[data-target="${path}"]`).classList.toggle('header_bottom_list_item_dropdown--active');
+    });
+  });
+
+  // Header 1024 Top Burger -----------------------------------------
+
+  let headerTopBurger = document.querySelectorAll('.header_top_burger');
+  let headerTopNav = document.querySelectorAll('.header_top_nav');
+  let headerTopBurgerClose = document.querySelectorAll('.header_top_nav_close');
+
+  headerTopBurger.forEach(function(burger){
+    burger.addEventListener('click', function(clickBurger){
+      headerTopNav.forEach(function(navOpen){
+        navOpen.classList.toggle('header_top_nav-active');
+      });
+    });
+  });
+  headerTopBurgerClose.forEach(function(burgerClose){
+    burgerClose.addEventListener('click', function(clickBurgerClose){
+      headerTopNav.forEach(function(navClose){
+        navClose.classList.toggle('header_top_nav-active');
+      });
+    });
+  });
+
+
+  // Header 1024 Top Search -----------------------------------------
+
+  let headerTopSearch = document.querySelectorAll('.header_top_search');
+  let headerTopSearchToggle = document.querySelectorAll('.header_top_search_toggle');
+  let headerTopSearchBtn = document.querySelectorAll('.top-search-btn');
+  let headerTopSearchInput = document.querySelectorAll('.top-search-input');
+
+  headerTopSearchToggle.forEach(function(toggle){
+    toggle.addEventListener('click', function(clickToggle){
+      headerTopSearch.forEach(function(search){
+        search.classList.toggle('header_top_search-active');
+      });
+      clickToggle.currentTarget.classList.toggle('header_top_search_toggle-active');
+      headerTopSearchBtn.forEach(function(searchBtn){
+        searchBtn.classList.toggle('top-search-btn-active');
+      });
+      headerTopSearchInput.forEach(function(searchInput){
+        searchInput.classList.toggle('top-search-input-active');
+      });
+    });
+  });
+
+  
+  //Selector-------------------------------------
 
   const element = document.querySelector('#selectFilter');
   const choices = new Choices(element, {
@@ -8,165 +81,248 @@ window.addEventListener('DOMContentLoaded', function () {
     shouldSort: false
   });
 
-  //Swiper
+  //Swiper (Hero)-------------------------------------
 
-  const swiper = new Swiper('.swiper-container', {
+  const swiper1 = new Swiper('.hero-swiper', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
-  
   });
 
-  //Gallery - Carousel (OwlCarousel) + https://codepen.io/Tolc/pen/OJXyKpo
+  //Gallery - Carousel-------------------------------------
 
-  $(document).ready(function() {
-    var el = $('.owl-carousel');
-    
-    var carousel;
-    var carouselOptions = {
-      margin: 20,
-      nav: true,
-      dots: false,
-      slideBy: 'page',
-      onInitialized: counter,
-      onChanged: counter,
-      responsive: {
-        0: {
-          items: 1,
-          rows: 1 //custom option not used by Owl Carousel, but used by the algorithm below
-        },
-        768: {
-          items: 2,
-          rows: 2 //custom option not used by Owl Carousel, but used by the algorithm below
-        },
-        991: {
-          items: 3,
-          rows: 2 //custom option not used by Owl Carousel, but used by the algorithm below
-        }
-      }
-    };
+  const swiper2 = new Swiper('.gallery-swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
 
+    pagination: {
+      el: '.gallery_paintings_nav_pag',
+      type: 'fraction',
+    },
 
-    function counter(event) {
-      if (!event.namespace) {
-        return;
-      }
-      var slides = event.relatedTarget;
-      $('.slider-counter').text(slides.relative(slides.current()) + 1 + '/' + slides.items().length);
+    breakpoints: {
+      319: {
+        slidesPerView: 1,
+        slidesPerGroup: 1,
+      },
+      
+      767: {
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+        spaceBetween: 38,
+      },
+      
+      1023: {
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+        spaceBetween: 34,
+      },
+
+      1919: {
+        slidesPerView: 3,
+        slidesPerGroup: 3,
+        spaceBetween: 50,
+      },
     }
 
-  
-    //Taken from Owl Carousel so we calculate width the same way
-    var viewport = function() {
-      var width;
-      if (carouselOptions.responsiveBaseElement && carouselOptions.responsiveBaseElement !== window) {
-        width = $(carouselOptions.responsiveBaseElement).width();
-      } else if (window.innerWidth) {
-        width = window.innerWidth;
-      } else if (document.documentElement && document.documentElement.clientWidth) {
-        width = document.documentElement.clientWidth;
-      } else {
-        console.warn('Can not detect viewport width.');
-      }
-      return width;
-    };
-  
-    var severalRows = false;
-    var orderedBreakpoints = [];
-    for (var breakpoint in carouselOptions.responsive) {
-      if (carouselOptions.responsive[breakpoint].rows > 1) {
-        severalRows = true;
-      }
-      orderedBreakpoints.push(parseInt(breakpoint));
-    }
-    
-    //Custom logic is active if carousel is set up to have more than one row for some given window width
-    if (severalRows) {
-      orderedBreakpoints.sort(function (a, b) {
-        return b - a;
+    // breakpoints: {
+    //   // when window width is >= 320px
+    //   320: {
+    //     slidesPerView: 1,
+    //   },
+    //   
+    //   768: {
+    //     slidesPerView: 2,
+    //     spaceBetween: 34,
+    //     grid: {
+    //       rows: 2,
+    //       },
+    //   },
+    //   
+    //   1024: {
+    //     slidesPerView: 2,
+    //     spaceBetween: 34,
+    //     grid: {
+    //       rows: 2,
+    //       },
+    //   },
+    //   // when window width is >= 1920px
+    //   1920: {
+    //     slidesPerView: 3,
+    //     spaceBetween: 50,
+    //     grid: {
+    //       rows: 2,
+    //       },
+    //   }
+    // },
+  });
+
+  //Gallery - Popup
+
+  let paintingLink = document.querySelectorAll('.gallery_paintings_slider_item');
+  let popupBlock = document.querySelectorAll('.gallery_popups'); 
+  let closeBtn = document.querySelectorAll('.gallery_popups_item_close');
+
+  paintingLink.forEach(function(painting){
+    painting.addEventListener('click', function(paintingClick){
+      const path = paintingClick.currentTarget.dataset.path;
+
+      popupBlock.forEach(function(popups){
+        popups.classList.remove('gallery_popups-active');
       });
-      var slides = el.find('[data-slide-index]');
-      var slidesNb = slides.length;
-      if (slidesNb > 0) {
-        var rowsNb;
-        var previousRowsNb = undefined;
-        var colsNb;
-        var previousColsNb = undefined;
-  
-        //Calculates number of rows and cols based on current window width
-        var updateRowsColsNb = function () {
-          var width =  viewport();
-          for (var i = 0; i < orderedBreakpoints.length; i++) {
-            var breakpoint = orderedBreakpoints[i];
-            if (width >= breakpoint || i == (orderedBreakpoints.length - 1)) {
-              var breakpointSettings = carouselOptions.responsive['' + breakpoint];
-              rowsNb = breakpointSettings.rows;
-              colsNb = breakpointSettings.items;
-              break;
-            }
-          }
-        };
-  
-        var updateCarousel = function () {
-          updateRowsColsNb();
-  
-          //Carousel is recalculated if and only if a change in number of columns/rows is requested
-          if (rowsNb != previousRowsNb || colsNb != previousColsNb) {
-            var reInit = false;
-            if (carousel) {
-              //Destroy existing carousel if any, and set html markup back to its initial state
-              carousel.trigger('destroy.owl.carousel');
-              carousel = undefined;
-              slides = el.find('[data-slide-index]').detach().appendTo(el);
-              el.find('.fake-col-wrapper').remove();
-              reInit = true;
-            }
-  
-  
-            //This is the only real 'smart' part of the algorithm
-  
-            //First calculate the number of needed columns for the whole carousel
-            var perPage = rowsNb * colsNb;
-            var pageIndex = Math.floor(slidesNb / perPage);
-            var fakeColsNb = pageIndex * colsNb + (slidesNb >= (pageIndex * perPage + colsNb) ? colsNb : (slidesNb % colsNb));
-  
-            //Then populate with needed html markup
-            var count = 0;
-            for (var i = 0; i < fakeColsNb; i++) {
-              //For each column, create a new wrapper div
-              var fakeCol = $('<div class="fake-col-wrapper"></div>').appendTo(el);
-              for (var j = 0; j < rowsNb; j++) {
-                //For each row in said column, calculate which slide should be present
-                var index = Math.floor(count / perPage) * perPage + (i % colsNb) + j * colsNb;
-                if (index < slidesNb) {
-                  //If said slide exists, move it under wrapper div
-                  slides.filter('[data-slide-index=' + index + ']').detach().appendTo(fakeCol);
-                }
-                count++;
-              }
-            }
-            //end of 'smart' part
-  
-            previousRowsNb = rowsNb;
-            previousColsNb = colsNb;
-  
-            if (reInit) {
-              //re-init carousel with new markup
-              carousel = el.owlCarousel(carouselOptions);
-            }
-          }
-        };
-  
-        //Trigger possible update when window size changes
-        $(window).on('resize', updateCarousel);
-  
-        //We need to execute the algorithm once before first init in any case
-        updateCarousel();
-      }
-    }
-  
-    //init
-    carousel = el.owlCarousel(carouselOptions);
+      document.querySelector(`[data-target="${path}"]`).classList.add('gallery_popups-active');
+    });
   });
+  closeBtn.forEach(function(close){
+    close.addEventListener('click', function(closeClick){
+      popupBlock.forEach(function(popups){
+        popups.classList.remove('gallery_popups-active');
+      });
+    });
+  });
+
+  //Accordion-------------------------------------
+
+  $( function() {
+    $( "#accordion" ).accordion({
+      hightStyle: "content"
+    });
+  });
+
+  //Accordion - Табы ---------------------------------
+  let catalogLink = document.querySelectorAll('.catalog_content_accordion_item_content');
+  let catalogTab = document.querySelectorAll('.catalog_content_artist');
+
+  catalogLink.forEach(function(link){
+    link.addEventListener('click', function(linkClick){
+      const path = linkClick.currentTarget.dataset.path;
+
+      catalogTab.forEach(function(tab){
+        tab.classList.remove('catalog_content_artist-active');
+      });
+      document.querySelector(`[data-target="${path}"]`).classList.add('catalog_content_artist-active');
+    });
+  });
+
+  //Events - Swiper --------------------------
+  const swiper3 = new Swiper('.events_cards-swiper', {
+
+    direction: 'horizontal',
+  
+    pagination: {
+      el: '.events_cards-swiper_pagination',
+    },
+  
+    navigation: {
+      nextEl: '.events_cards-swiper_btns_next',
+      prevEl: '.events_cards-swiper_btns_prev',
+    },
+
+    breakpoints: {
+      319: {
+        slidesPerView: 1,
+        slidesPerGroup: 1,
+      },
+      
+      767: {
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+        spaceBetween: 34,
+      },
+      
+      1023: {
+        slidesPerView: 3,
+        slidesPerGroup: 3,
+        spaceBetween: 27,
+      },
+
+      1919: {
+        slidesPerView: 3,
+        slidesPerGroup: 3,
+        spaceBetween: 50,
+      },
+    }
+  });
+
+  // Projects - Swiper --------------------
+
+  const swiper4 = new Swiper('.projects_partners_links_swiper', {
+
+    direction: 'horizontal',
+  
+    navigation: {
+      nextEl: '.projects_partners_links_swiper_btn-next',
+      prevEl: '.projects_partners_links_swiper_btn-prev',
+    },
+
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        slidesPerGroup: 1,
+      },
+      
+      768: {
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+        spaceBetween: 34,
+      },
+      
+      1024: {
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+        spaceBetween: 50,
+      },
+
+      1920: {
+        slidesPerView: 3,
+        slidesPerGroup: 3,
+        spaceBetween: 50,
+      },
+    }
+  });
+
+  // Contacts - Map -----------------------
+  
+  ymaps.ready(init);
+  function init(){
+    
+    var myMap = new ymaps.Map("contacts_map", {
+      center: [55.76025106, 37.61447975],
+      zoom: 14,
+      controls: []
+    });
+
+    myMap.controls.add('zoomControl', {
+      size: 'small',
+      float: 'right',
+      position: {
+          right: '10px',
+          top: '260px'
+      }
+    });
+    
+    myMap.controls.add('geolocationControl', {
+      float: 'right',
+      position: {
+        right: '10px',
+        top: '350px'
+    }
+    });
+
+    var myPlacemark = new ymaps.Placemark([55.76025106, 37.61447975], {}, {
+      iconLayout: 'default#image',
+      iconImageHref: '../img/8_Contacts/Placemark.svg',
+      iconImageSize: [20, 20],
+      iconImageOffset: [0, 0]
+    });
+    
+    myMap.geoObjects.add(myPlacemark);
+  }
 
 });
